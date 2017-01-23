dcef3d2d1d17a7f79758f17ac94d7a7357669353
~sabio~ <~kirubba@naver.com~>
2017-01-23 14:39:15 +0900

    중간점검 및 멀쥐

--- Cafe_Board/src/main/java/com/sabio/cafe/board/controller/BoardController.java
+++ Cafe_Board/src/main/java/com/sabio/cafe/board/controller/BoardController.java
@@ -3,9 +3,9 @@ package com.sabio.cafe.board.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sabio.cafe.board.dto.BoardDto;
@@ -24,9 +24,9 @@ public class BoardController {
 		return mView;
 	}
 	
-	@RequestMapping("/cafe/private/insertform")
-	public String insertForm(){
-		return "cafe/private/insertform";
+	@RequestMapping("/cafe/private/{url}")
+	public void insertForm(@PathVariable String url){
+	
 	}
 	
 	@RequestMapping("/cafe/private/insert")
